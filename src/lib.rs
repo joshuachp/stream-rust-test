a42ed01932039f749eed379fdc2d804f6f1cb134
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-08-20 16:26:10 +0200

    feat(test): one plus one
    Signed-off-by: Joshua Chapman <joshua.chapman@joshuachp.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,17 @@
 // Copyright 2024 SECO Mind Srl
 //
 // SPDX-License-Identifier: Apache-2.0
+
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::sum;
+
+    #[test]
+    fn one_plus_one() {
+        assert_eq!(sum(1, 1), 2)
+    }
+}
